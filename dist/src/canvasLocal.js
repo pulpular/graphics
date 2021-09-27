705c622d4e0f387615073e090235983dadbb9cba
Jorge Cruz <jorrge.cruz@gmail.com>
2021-09-27 11:42:16 -0500

    Cambios de la funcion del graf

--- dist/src/canvasLocal.js
+++ dist/src/canvasLocal.js
@@ -1,8 +1,8 @@
 export class CanvasLocal {
     constructor(g, canvas) {
         this.graphics = g;
-        this.rWidth = 6;
-        this.rHeight = 4;
+        this.rWidth = 12;
+        this.rHeight = 10;
         this.maxX = canvas.width - 1;
         this.maxY = canvas.height - 1;
         this.pixelSize = Math.max(this.rWidth / this.maxX, this.rHeight / this.maxY);
@@ -19,36 +19,35 @@ export class CanvasLocal {
         this.graphics.stroke();
     }
     fx(x) {
-        return Math.sin(x * 2.5);
+        return (Math.sin(x));
     }
     paint() {
-        this.drawLine(this.iX(-3), this.iY(0), this.iX(3), this.iY(0));
-        this.drawLine(this.iX(0), this.iY(2), this.iX(0), this.iY(-2));
-        this.drawLine(this.iX(-3), this.iY(0), this.iX(3), this.iY(0));
-        this.drawLine(this.iX(0), this.iY(2), this.iX(0), this.iY(-2));
         //dibuja la cuadricula
         this.graphics.strokeStyle = 'lightgray';
-        for (let x = -3; x <= 3; x += 0.25) {
-            this.drawLine(this.iX(x), this.iY(-2), this.iX(x), this.iY(2));
+        for (let x = -5; x <= 5; x += 1) {
+            this.drawLine(this.iX(x), this.iY(-5), this.iX(x), this.iY(5));
         }
-        for (let y = -2; y <= 2; y += 0.25) {
-            this.drawLine(this.iX(-3), this.iY(y), this.iX(3), this.iY(y));
+        for (let y = -5; y <= 5; y += 1) {
+            this.drawLine(this.iX(-5), this.iY(y), this.iX(5), this.iY(y));
         }
-        //dibuja las divisiones
         this.graphics.strokeStyle = 'black';
-        for (let x = -3; x <= 3; x++) {
+        this.drawLine(this.iX(-5), this.iY(0), this.iX(5), this.iY(0));
+        this.drawLine(this.iX(0), this.iY(5), this.iX(0), this.iY(-5));
+        //dibuja las divisiones
+        for (let x = -5; x <= 5; x++) {
             this.drawLine(this.iX(x), this.iY(-0.1), this.iX(x), this.iY(0.1));
-            this.graphics.strokeText(x + "", this.iX(x - 0.1), this.iY(-0.2));
+            this.graphics.strokeText(x + "", this.iX(x - 0.1), this.iY(-0.3));
         }
-        for (let y = -2; y <= 2; y++) {
+        for (let y = -5; y <= 5; y++) {
             this.drawLine(this.iX(-0.1), this.iY(y), this.iX(0.1), this.iY(y));
+            this.graphics.strokeText(y + "", this.iX(0.2), this.iY(y));
         }
-        this.graphics.strokeText("X", this.iX(2.9), this.iY(0.2));
-        this.graphics.strokeText("Y", this.iX(-0.2), this.iY(1.8));
+        this.graphics.strokeText("X", this.iX(4.8), this.iY(0.2));
+        this.graphics.strokeText("Y", this.iX(-0.2), this.iY(4.8));
         //dibujar la funcion
         this.graphics.strokeStyle = 'red';
         let paso = 0.1;
-        for (let x = -3; x <= 3; x += paso) {
+        for (let x = -5; x <= 5; x += paso) {
             this.drawLine(this.iX(x), this.iY(this.fx(x)), this.iX(x + paso), this.iY(this.fx(x + paso)));
         }
         /*this.graphics.strokeStyle = 'red';
