aa6dfa8596ae635b5f18355454882eeeb2deda1d
Jorge Cruz <jorrge.cruz@gmail.com>
2021-10-08 11:33:40 -0500

    Cambios de dibujo de rectangulo

--- dist/src/index.js
+++ dist/src/index.js
@@ -4,4 +4,19 @@ let graphics;
 canvas = document.getElementById('circlechart');
 graphics = canvas.getContext('2d');
 const miCanvas = new CanvasLocal(graphics, canvas);
-miCanvas.paint();
+let dibujar = false;
+let x1, y1;
+//miCanvas.paint();
+function movimiento(evt) {
+    graphics.clearRect(0, 0, 640, 480);
+    if (!dibujar) {
+        x1 = evt.offsetX;
+        y1 = evt.offsetY;
+        dibujar = true;
+    }
+    else {
+        graphics.strokeRect(x1, y1, Math.abs(x1 - evt.offsetX), Math.abs(y1 - evt.offsetY));
+        dibujar = false;
+    }
+}
+canvas.addEventListener('click', movimiento);
