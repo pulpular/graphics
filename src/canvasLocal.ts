e30bf06c1ddbd35d96c6b9d0744c107d5b11cc21
Jorge Cruz <jorrge.cruz@gmail.com>
2021-10-11 11:58:01 -0500

    Versio del juego de la vida

--- src/canvasLocal.ts
+++ src/canvasLocal.ts
@@ -1,3 +1,4 @@
+import { Play } from "./play.js";
 
 export class CanvasLocal {
   //atributos
@@ -10,18 +11,25 @@ export class CanvasLocal {
   protected centerX: number;
   protected centerY: number;
   step: number;
+  mallaSize: number;
+  juego: Play;
+  blockSize: number;
   
-      
   public constructor(g: CanvasRenderingContext2D, canvas: HTMLCanvasElement){
     this.graphics = g;
-    this.rWidth = 6;
-    this.rHeight= 4;
+    this.mallaSize = 50;
+    this.rWidth = this.mallaSize*1.33;
+    this.rHeight= this.mallaSize;
     this.maxX = canvas.width - 1
     this.maxY = canvas.height - 1;
     this.pixelSize = Math.max(this.rWidth / this.maxX, this.rHeight / this.maxY);
-    this.centerX = this.maxX/2;
-    this.centerY = this.maxY / 2;
+    this.centerX = this.maxX/12*1.5;
+    this.centerY = this.maxY-2;
     this.step = 0;
+    
+    this.blockSize = this.iX(1) - this.iX(0);
+    this.juego = new Play(this.mallaSize);
+		this.juego.generaMatriz2();
   }
 
   iX(x: number):number{return Math.round(this.centerX + x/this.pixelSize);}
@@ -44,43 +52,31 @@ export class CanvasLocal {
   }
 
   paint() {
+    let res: number[][];
     
-    this.drawLine(this.iX(-3), this.iY(0), this.iX(3), this.iY(0));
-    this.drawLine(this.iX(0), this.iY(2), this.iX(0), this.iY(-2));
-
-    this.drawLine(this.iX(-3), this.iY(0), this.iX(3), this.iY(0));
-    this.drawLine(this.iX(0), this.iY(2), this.iX(0), this.iY(-2));
-
     //dibuja la cuadricula
+    this.graphics.fillStyle = 'white';
+    this.graphics.fillRect(0, 0, this.maxX, this.maxY);
     this.graphics.strokeStyle = 'lightgray';
-    for (let x = -3; x <= 3; x+=0.25){
-      this.drawLine(this.iX(x), this.iY(-2), this.iX(x), this.iY(2));
+   /* for (let x = 0; x <= this.mallaSize; x++){
+      this.drawLine(this.iX(x), this.iY(0), this.iX(x), this.iY(this.mallaSize));
     }
-    for (let y = -2; y <= 2; y+=0.25){
-      this.drawLine(this.iX(-3), this.iY(y), this.iX(3), this.iY(y));
-    }
-    //dibuja las divisiones
-    this.graphics.strokeStyle = 'black';
-    for (let x = -3; x <= 3; x++){
-      this.drawLine(this.iX(x), this.iY(-0.1), this.iX(x), this.iY(0.1));
-      this.graphics.strokeText(x+"", this.iX(x-0.1), this.iY(-0.2));
-    }
-    for (let y = -2; y <= 2; y++){
-      this.drawLine(this.iX(-0.1), this.iY(y), this.iX(0.1), this.iY(y));
-    }
-    this.graphics.strokeText("X", this.iX(2.9), this.iY(0.2));
-    this.graphics.strokeText("Y", this.iX(-0.2), this.iY(1.8));
-    //dibujar la funcion
-    this.graphics.strokeStyle = 'red';
-    let paso: number = 0.1;
-    for (let x = -3; x <= 3; x+=paso){
-      this.drawLine(this.iX(x), this.iY(this.fx(x)), this.iX(x+paso), this.iY(this.fx(x+paso)));
-    }
-    /*this.graphics.strokeStyle = 'red';
-    this.drawLine(this.iX(0), this.iY(0), this.iX(2), this.iY(0));
-    this.drawLine(this.iX(2), this.iY(0), this.iX(1), this.iY(1.5));
-    this.drawLine(this.iX(1), this.iY(1.5), this.iX(0), this.iY(0));*/
+    for (let y = 0; y <= this.mallaSize; y++){
+      this.drawLine(this.iX(0), this.iY(y), this.iX(this.mallaSize), this.iY(y));
+    }*/
     
+    //trazar el juego (los cuadritos) a partir de la matriz creada
+    res = this.juego.getMat();
+    this.graphics.fillStyle = 'black';
+		for(let i=0; i<this.mallaSize; i++){
+			for(let j=0;j<this.mallaSize; j++){	 
+        if (res[i][j] == 1)
+          this.graphics.fillRect(this.iX(i), this.iY(j), this.blockSize, this.blockSize);
+      }
+    }
+    //actualizar la matriz para la siguiente pasada
+		res=this.juego.juegoDeLaVida(this.juego.getMat());
+		this.juego.setMat(res);
   }
 
 }
