b2b8dea6c3008805a75490173b229049e12a7d6b
Jorge Cruz <jorrge.cruz@gmail.com>
2022-09-19 13:43:53 -0500

    Salto de rama

--- src/canvasLocal.ts
+++ src/canvasLocal.ts
@@ -33,13 +33,18 @@ export class CanvasLocal {
     this.graphics.stroke();
   }
 
+  drawOval(x1: number, y1: number, r:number) {
+    this.graphics.beginPath();
+    this.graphics.arc(x1, y1, r, 0, 2 * Math.PI, false);
+    this.graphics.fill();
+  }
+
   fx(x:number):number {
     return (Math.sin(x));
   }
 
 
   paint() {
-    
     
 
     //dibuja la cuadricula
@@ -50,26 +55,36 @@ export class CanvasLocal {
     for (let y = -5; y <= 5; y+=1){
       this.drawLine(this.iX(-5), this.iY(y), this.iX(5), this.iY(y));
     }
-    this.graphics.strokeStyle = 'black';
-    this.drawLine(this.iX(-5), this.iY(0), this.iX(5), this.iY(0));
-    this.drawLine(this.iX(0), this.iY(5), this.iX(0), this.iY(-5));
+    
     //dibuja las divisiones
     
     for (let x = -5; x <= 5; x++){
-      this.drawLine(this.iX(x), this.iY(-0.1), this.iX(x), this.iY(0.1));
-      this.graphics.strokeText(x+"", this.iX(x-0.1), this.iY(-0.3));
+      this.graphics.strokeStyle = 'green';
+      this.drawLine(this.iX(x), this.iY(-0.1), this.iX(x), this.iY(0));
+      this.graphics.strokeStyle = 'blue';
+      this.graphics.strokeText(x + "", this.iX(x-0.05), this.iY(-0.4));
     }
     for (let y = -5; y <= 5; y++){
-      this.drawLine(this.iX(-0.1), this.iY(y), this.iX(0.1), this.iY(y));
-      this.graphics.strokeText(y+"", this.iX(0.2), this.iY(y));
+      this.drawLine(this.iX(-0.1), this.iY(y), this.iX(0), this.iY(y));
+      this.graphics.strokeText(y+"", this.iX(-0.3), this.iY(y-0.05));
     }
+    this.graphics.strokeStyle = 'green';
+    this.drawLine(this.iX(-5), this.iY(0), this.iX(5), this.iY(0));
+    this.drawLine(this.iX(0), this.iY(-5), this.iX(0), this.iY(5));
+    
     this.graphics.strokeText("X", this.iX(4.8), this.iY(0.2));
     this.graphics.strokeText("Y", this.iX(-0.2), this.iY(4.8));
     //dibujar la funcion
-    this.graphics.strokeStyle = 'red';
+    this.graphics.strokeStyle = 'blue';
+    this.graphics.fillStyle = 'blue';
     let paso: number = 0.1;
+    let cont=0
     for (let x = -5; x <= 5; x+=paso){
       this.drawLine(this.iX(x), this.iY(this.fx(x)), this.iX(x+paso), this.iY(this.fx(x+paso)));
+      if (cont%10 === 0) {
+        this.drawOval(this.iX(x), this.iY(this.fx(x)), this.iX(0.1)-this.iX(0));
+      }  
+      cont++;
     }
     /*this.graphics.strokeStyle = 'red';
     this.drawLine(this.iX(0), this.iY(0), this.iX(2), this.iY(0));
