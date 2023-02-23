07da3ea007202ddeb0e4393bbdd9190dd7e4403a
Jorge Cruz <jorrge.cruz@gmail.com>
2023-02-23 12:20:36 -0600

    Este commit rs temporal

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,10 +1,21 @@
-import { CanvasLocal } from './canvasLocal.js';
+//import { CanvasLocal } from './canvasLocal.js';
 let canvas;
 let graphics;
 canvas = document.getElementById('circlechart');
 graphics = canvas.getContext('2d');
-const miCanvas = new CanvasLocal(graphics, canvas);
-miCanvas.paint();
+function drawLine(x1, y1, x2, y2) {
+    graphics.beginPath();
+    graphics.moveTo(x1, y1);
+    graphics.lineTo(x2, y2);
+    graphics.closePath();
+    graphics.stroke();
+}
+//graphics.fillRect(-50, 240.8, 100, 50);
+drawLine(100, 100, 200, 100);
+drawLine(200, 100, 150, 150);
+drawLine(150, 150, 100, 100);
+//const miCanvas:CanvasLocal = new CanvasLocal(graphics, canvas);
+//miCanvas.paint();
 /*let dibujar = false
 let x1, y1;
 //
